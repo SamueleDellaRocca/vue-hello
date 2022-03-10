b4187daff3794e8c5e3e0de5ba8a2c17e448d0d7
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-10 15:27:58 +0100

    seconda img

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ const app = new Vue(
             messaggio: 'forza la magica roma',
             classe: 'scritta',
             image: 'img/totti.jpg',
+            inputText: 'daje roma',
+            classinput: 'input',
+            image2: 'https://cloud.rtl.it/RTLFM/News/Article/600x600/mi-chiamo-francesco-totti-sara-presentato-alla-festa-del-cinema-di-roma-il-docufilm-sulla-vita-de-ev47s.jpg',
         },
     }
 )
